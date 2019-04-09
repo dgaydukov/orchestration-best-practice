9fb37c5fe70775fe688c7b9fca7335b14e5428e2
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-04-09 17:36:46 +0800

    add notes table migration

---
+++ backend/src/migration/1554801587318-add-table-notes.ts
@@ -0,0 +1,36 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class addTableNotes1554801587318 implements MigrationInterface {
+
+    async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.createTable(new Table({
+            name: 'notes',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isGenerated: true,
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                },
+                {
+                    name: 'title',
+                    type: 'varchar',
+                },
+                {
+                    name: 'desc',
+                    type: 'varchar',
+                },
+                {
+                    name: 'date',
+                    type: 'date',
+                },
+            ],
+        }));
+    }
+
+    async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropTable('notes');
+    }
+
+}
