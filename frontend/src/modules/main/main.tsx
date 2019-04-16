b847eccf93e26feb37b02580b031cc77cf43dffd
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-04-16 18:13:20 +0800

    add note done

--- frontend/src/modules/main/main.tsx
+++ frontend/src/modules/main/main.tsx
@@ -2,15 +2,31 @@
 
 
 import * as React from "react";
+import {createNote} from '../../api/webapi';
 
 interface IProps {
     getAllNotes: Function;
     notes: [],
 }
 
-class Main extends React.PureComponent<IProps, any>{
+class Main extends React.PureComponent<IProps>{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            title: "",
+            desc: "",
+        }
+        this.addNote = this.addNote.bind(this);
+        this.changeState = this.changeState.bind(this);
+    }
     componentDidMount(){
-        this.props.getAllNotes()
+        this.props.getAllNotes();
+    }
+
+    changeState(e){
+        console.log(this.state)
+        this.state[e.target.name] = e.target.value;
     }
 
     render(){
@@ -29,9 +45,20 @@ class Main extends React.PureComponent<IProps, any>{
                 </div>
                 <div>
                     Add new note
+                    <div><label>Title: <input type="text" name="title" onChange={this.changeState}/></label></div>
+                    <div><label>Desc: <input type="text" name="desc" onChange={this.changeState}/></label></div>
+                    <div><label><button onClick={this.addNote}>add</button></label></div>
                 </div>
             </div>
         )
+    }
+
+    async addNote(){
+        const note = await createNote(this.state)
+        if(note.status === 200){
+            alert(`Added new item`);
+            this.props.getAllNotes();
+        }
     }
 }
 export default Main;
